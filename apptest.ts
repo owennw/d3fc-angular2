0e786b768cb1205a9b42bb06e6012068d4cc167c
Nick Owen <nowen@scottlogic.com>
2015-11-24 15:24:47 +0000

    Spike #1: working angular 2

---
+++ apptest.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path="./typings/node/node.d.ts" />
+
+import {bootstrap, Component, CORE_DIRECTIVES} from 'angular2/angular2';
+
+@Component({
+    selector: 'hello-app',
+    template: `
+        <h1>{{title}}</h1>
+        <ul>
+            <li *ng-for="#framework of frameworks">
+                {{framework}}
+            </li>
+        </ul>
+    `,
+    directives: [CORE_DIRECTIVES]
+})
+
+export class HelloApp {
+    public title: string = 'Hello World!';
+    public frameworks: string[] = ['Angular2', 'React'];
+}
+
+bootstrap(HelloApp);
